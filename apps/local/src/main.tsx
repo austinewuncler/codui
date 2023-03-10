24e550ed1cc8803a7626b4a6b28d59dcc596178f
Austine Were <austinewuncler@gmail.com>
2023-03-10 13:03:54 +0300

    refactor(local): establish project structure

--- apps/local/src/main.tsx
+++ apps/local/src/main.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './components';
+import { App } from './common/components';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
