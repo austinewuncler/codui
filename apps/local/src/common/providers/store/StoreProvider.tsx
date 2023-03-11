8626a13e15dce326bc0a5be3ca30e1b688fd2bad
Austine Were <austinewuncler@gmail.com>
2023-03-11 11:43:48 +0300

    fix(local): prevent loading of global state on file change

--- apps/local/src/common/providers/store/StoreProvider.tsx
+++ apps/local/src/common/providers/store/StoreProvider.tsx
@@ -1,12 +1,14 @@
-import React, { type PropsWithChildren } from 'react';
+import React, { type PropsWithChildren, useMemo } from 'react';
 import { Provider } from 'react-redux';
 
 import { setupStore } from './store';
 
 type Props = PropsWithChildren;
 
-const StoreProvider = ({ children }: Props): JSX.Element => (
-  <Provider store={setupStore()}>{children}</Provider>
-);
+const StoreProvider = ({ children }: Props): JSX.Element => {
+  const store = useMemo(() => setupStore(), []);
+
+  return <Provider store={store}>{children}</Provider>;
+};
 
 export default StoreProvider;
