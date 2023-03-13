5a8c8fd280fd8906e68720be1f72e9113fd2e79a
Austine Were <austinewuncler@gmail.com>
2023-03-14 00:16:53 +0300

    feat(editor): set editor colors

--- packages/components/editor/src/themes/dark.theme.ts
+++ packages/components/editor/src/themes/dark.theme.ts
@@ -1,10 +1,20 @@
 import { EditorView } from 'codemirror';
 
+const background = '#000';
+const cursor = '#80a4c2';
+
 const darkTheme = EditorView.theme(
   {
-    '&': {
-      color: '#c8c8c8',
-      background: '#000',
+    '&': { color: '#c8c8c8', background },
+    '.cm-content': { caretColor: cursor },
+    '&.cm-focused .cm-cursor': { borderLeftColor: cursor },
+    '&.cm-focused .cm-selectionBackground, ::selection': {
+      backgroundColor: '#5f7e9779',
+    },
+    '.cm-gutters': {
+      backgroundColor: background,
+      color: '#4b6479',
+      border: 'none',
     },
   },
   { dark: true }
--- packages/components/editor/src/themes/light.theme.ts
+++ packages/components/editor/src/themes/light.theme.ts
@@ -1,7 +1,20 @@
 import { EditorView } from 'codemirror';
 
+const background = '#fff';
+const cursor = '#90A7B2';
+
 const lightTheme = EditorView.theme({
-  '&': { color: '#403f53', backgroundColor: '#fff' },
+  '&': { color: '#403f53', backgroundColor: background },
+  '.cm-content': { caretColor: cursor },
+  '&.cm-focused .cm-cursor': { borderLeftColor: cursor },
+  '&.cm-focused .cm-selectionBackground, ::selection': {
+    backgroundColor: '#339cec33',
+  },
+  '.cm-gutters': {
+    backgroundColor: background,
+    color: cursor,
+    border: 'none',
+  },
 });
 
 export default lightTheme;
